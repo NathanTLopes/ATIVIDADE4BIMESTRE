703f6ff30b8778cfb4bd67bd2b21e63ad9a3f6c4
NathanTLopes <texeiranathan20@gmail.com>
2025-02-09 01:36:47 -0400

    att

--- Clinica Médica/src/app/Medicos/page.js
+++ Clinica Médica/src/app/Medicos/page.js
@@ -3,57 +3,53 @@ import React, { useState, useEffect } from "react";
 const urlPadrao = "https://api-clinica-2a.onrender.com";
 import styles from "./medicos.module.css";
 
-export default function medicos() {
+export default function Medicos() {
     const [medicos, setMedicos] = useState([]);
     const [medicosPorNome, setMedicosPorNome] = useState([]);
     const [showListaDePesquisa, setShowListaDePesquisa] = useState(false);
 
     async function apresetarTodosMedicos() {
         try {
-            const response = await fetch(`${urlPadrao}/medicos`)
+            const response = await fetch(`${urlPadrao}/medicos`);
             if (!response.ok) {
                 throw new Error("Erro ao buscar dados:" + response.statusText);
-
             }
             const data = await response.json();
             setMedicos(data);
-
         } catch (error) {
-            console.log('Ocorreu algum erro:' + error)
+            console.log("Ocorreu algum erro:" + error);
         }
     }
 
     async function pesquisarMedicoPorNome(nome) {
-
         try {
-            const response = await fetch(`${urlPadrao}/medicos?nome=${nome}`)
+            const response = await fetch(`${urlPadrao}/medicos?nome=${nome}`);
             if (!response.ok) {
                 throw new Error("Erro ao buscar dados:" + response.statusText);
-
             }
             const data = await response.json();
             setMedicosPorNome(data);
-
         } catch (error) {
-            console.log('Ocorreu algum erro:' + error)
+            console.log("Ocorreu algum erro:" + error);
         }
     }
 
     useEffect(() => {
         apresetarTodosMedicos();
-        pesquisarMedicoPorNome('')
-    }, [])
+        pesquisarMedicoPorNome("");
+    }, []);
 
     return (
         <div className={styles.container}>
+            <h2 className={styles.titulo}>Lista de Médicos</h2>
             <div className={styles.containerPesquisa}>
-                <button className={styles.botaoPesquisar} onClick={() => { setShowListaDePesquisa(!showListaDePesquisa) }}>Pesquisar médico</button>
+                <button className={styles.botaoPesquisar} onClick={() => { setShowListaDePesquisa(!showListaDePesquisa); }}>Buscar médico</button>
                 {showListaDePesquisa && (
                     <>
-                    <div className={styles.fundoPopUp} onClick={() => setShowListaDePesquisa(false)}></div>
-                    <div className={styles.containerListaPesquisa}>
-                        <div className={styles.containerInput}>
-                            <input type="text" onChange={(e) => pesquisarMedicoPorNome(e.target.value)} placeholder="Pesquisar médico" className={styles.input} onInput={(e) => e.target.value = e.target.value.replace(/['"]/g, '')}/>
+                        <div className={styles.fundoPopUp} onClick={() => setShowListaDePesquisa(false)}></div>
+                        <div className={styles.containerListaPesquisa}>
+                            <div className={styles.containerInput}>
+                                <input type="text" onChange={(e) => pesquisarMedicoPorNome(e.target.value)} placeholder="Pesquisar médico" className={styles.input} onInput={(e) => e.target.value = e.target.value.replace(/['"]/g, '')} />
                             </div>
                             <ul className={styles.listaPesquisa}>
                                 {medicosPorNome.map((medico) => (
@@ -69,10 +65,10 @@ export default function medicos() {
                     <thead className={styles.cabecalho}>
                         <tr className={styles.linhaCabecalho}>
                             <th>ID</th>
-                            <th>Nome</th>
-                            <th>Telefone</th>
-                            <th>Email</th>
-                            <th>Especialidade</th>
+                            <th>NOME</th>
+                            <th>TELEFONE</th>
+                            <th>EMAIL</th>
+                            <th>ESPECIALIZADOS</th>
                         </tr>
                     </thead>
                     <tbody className={styles.corpoTabela}>
