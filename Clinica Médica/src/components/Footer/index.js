543ff0c0bbaad2f0a2dd6f7b0336cde6cc512561
NathanTLopes <texeiranathan20@gmail.com>
2025-02-12 08:03:40 -0400

    att

--- Clinica Médica/src/components/Footer/index.js
+++ Clinica Médica/src/components/Footer/index.js
@@ -4,7 +4,7 @@ import styles from "./Footer.module.css";
 import Link from 'next/link';
 import Image from 'next/image';
 const Footer = () => {
-  return <footer className={styles.footer}>FOOTERRRRRRR</footer>;
+  return <footer className={styles.footer}>© 2025 Nathan Teixeira Lopes. Todos os direitos são reservados.</footer>;
 };
 
 export default Footer;
